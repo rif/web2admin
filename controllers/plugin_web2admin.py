23388c9d92ece7d1ddddfe1021a8f3e6ce305e3c
Radu Ioan Fericean <radu@fericean.ro>
2012-09-10 17:44:44 +0300

    worked on possible multi delete feature

--- controllers/plugin_web2admin.py
+++ controllers/plugin_web2admin.py
@@ -12,20 +12,32 @@ def  view_table():
                              searchable = check_access(table, 'w2a_select'),
                              editable = check_access(table, 'w2a_edit'),
                              deletable = check_access(table, 'w2a_delete'),
+                             #selectable = lambda ids: del_action(table, ids)
     )
     return locals()
+
+def del_action(table, ids):
+    if not ids:
+        response.flash=T('Please select some rows to delete')
+    else:
+        to_delete = db(db[table].id.belongs(ids))
+        to_delete.delete()
+
 
 @auth.requires_membership('w2a_root')
 def permissions():
     form = SQLFORM.factory(
         Field('action',
-              requires=IS_IN_SET((('add',T('Add permissions')), ('remove', T('Remove permissions'))), zero=None), default=True, widget=SQLFORM.widgets.options.widget),
+              requires=IS_IN_SET((('add',T('Add permissions')), ('remove', T('Remove permissions'))), zero=None),
+              default=True,
+              widget=SQLFORM.widgets.options.widget),
         Field('users', 'list:int',
               requires = IS_IN_DB(db, 'auth_user.id', '%(first_name)s %(last_name)s', zero=T('choose one'), multiple=True)),
         Field('tables', 'list:string',
               requires=IS_IN_SET(db.tables, zero=T('choose one'), multiple=True)),
         Field('permissions', 'list:string',
-              requires= IS_IN_SET(('w2a_read', 'w2a_create', 'w2a_select', 'w2a_edit', 'w2a_delete'), multiple=True ), widget=SQLFORM.widgets.checkboxes.widget)
+              requires= IS_IN_SET(('w2a_read', 'w2a_create', 'w2a_select', 'w2a_edit', 'w2a_delete'), multiple=True ),
+              widget=SQLFORM.widgets.checkboxes.widget)
     )
     if form.process().accepted:
         response.flash = T('permissions granted') if form.vars.action == 'add' else T('permissions removed')
