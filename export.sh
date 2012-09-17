9a6907483edff248740e0a62c3c2fb5651270a4b
Radu Ioan Fericean <radu@fericean.ro>
2012-09-17 17:21:17 +0300

    exporting script

---
+++ export.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env sh
+
+cp controllers/plugin_web2admin.py ~/Documents/webframeworks/web2admin/controllers/
+cp models/plugin_web2admin.py ~/Documents/webframeworks/web2admin/models/
+cp -r models/plugin_web2admin ~/Documents/webframeworks/web2admin/models/
+cp -r views/plugin_web2admin ~/Documents/webframeworks/web2admin/views
+cp -r static/plugin_web2admin ~/Documents/webframeworks/web2admin/static
