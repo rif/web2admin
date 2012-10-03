5c19d856e41c1748fb50e4f9a28829c5e36c2a14
Radu Ioan Fericean <radu@fericean.ro>
2012-10-03 23:27:31 +0300

    hello ubuntu one

--- release.sh
+++ release.sh
@@ -2,6 +2,6 @@
 
 git co master
 git merge devel
-tar czvf ~/box/web2py.plugin.web2admin.w2p --exclude='*.sh' *
+tar czvf ~/Ubuntu\ One/web2py.plugin.web2admin.w2p --exclude='*.sh' *
 git co devel
 git push
