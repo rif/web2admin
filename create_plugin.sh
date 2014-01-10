692780b8e57ab9c27157fa5de014f4bfaaf10b2e
Radu Ioan Fericean <radu@fericean.ro>
2014-01-10 14:45:49 +0200

    generated new w2p file

--- create_plugin.sh
+++ create_plugin.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env sh
 
+rm web2py.plugin.web2admin.w2p
 tar czvf web2py.plugin.web2admin.w2p --exclude='*.sh' *
--- release.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env sh
-
-git co master
-git merge devel
-git co devel
-git push
